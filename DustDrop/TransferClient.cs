7680585b8414e15554871f775b2ea90fb5b75b4c
Duoqi Zheng <zhedy005@mymail.unisa.edu.au>
2019-07-05 01:32:17 +0930

    Add project

---
+++ DustDrop/TransferClient.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DustDrop
+{
+    public delegate void TransferEventHandler(object sender, TransferQueue queue);
+    public delegate void ConnectCallBack(object sender, string error);
+    public class TransferClient
+    {
+        private Socket _BaseSocket;
+        private byte[] _Buffer = new byte[8192];
+        private ConnectCallBack _ConnectCallback;
+        private Dictionary<int, TransferQueue> _Transfers = new Dictionary<int, TransferQueue>();
+        public Dictionary<int, TransferQueue> Transfers
+        {
+            get{return _Transfers;}
+        }
+
+        public bool Closed
+        {
+            get;
+            private set;
+        }
+
+        public string OutputFolder
+        {
+            get;
+            set;
+        }
+
+        public IPEndPoint EndPoint
+        {
+            get;
+            private set;
+        }
+
+        public event TransferEventHandler Queued;
+        public event TransferEventHandler ProgressChanged;
+        public event TransferEventHandler Stopped;
+        public event TransferEventHandler Complate;
+        public event EventHandler Disconnected;
+
+        public TransferClient()
+        {
+            _BaseSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public TransferClient(Socket socket)
+        {
+            _BaseSocket = socket;
+            EndPoint = (IPEndPoint)_BaseSocket.RemoteEndPoint;
+        }
+
+        public void Connect(string hostname, int port, ConnectCallBack callBack)
+        {
+            _ConnectCallback = callBack;
+
+            _BaseSocket.BeginConnect(hostname, port, connectCallback, null);
+        }
+
+        private void connectCallback(IAsyncResult ar)
+        {
+            string error = null;
+            try
+            {
+                _BaseSocket.EndConnect(ar);
+                EndPoint = (IPEndPoint)_BaseSocket.RemoteEndPoint;
+            } catch (Exception ex)
+            {
+                error = ex.Message;
+            }
+            _ConnectCallback(this, error);
+        }
+
+        public void Run()
+        {
+            try
+            {
+                _BaseSocket.BeginReceive(_Buffer, 0, _Buffer.Length, SocketFlags.Peek, null, null);
+            } catch
+            {
+
+            }
+        }
+
+        public void Close()
+        {
+            Closed = true;
+            _BaseSocket.Close();
+            _Transfers.Clear();
+            _Transfers = null;
+            _Buffer = null;
+            OutputFolder = null;
+            Disconnected?.Invoke(this, EventArgs.Empty);
+        }
+        public void Send(byte[] data)
+        {
+            if (Closed)
+            {
+                return;
+            }
+            lock (this)
+            {
+                try
+                {
+                    _BaseSocket.Send(BitConverter.GetBytes(data.Length), 0, 4, SocketFlags.None);
+                    _BaseSocket.Send(data, 0, data.Length, SocketFlags.None);
+                } catch
+                {
+                    Close();
+                }
+            }
+        }
+        private void receiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                int found = _BaseSocket.EndReceive(ar);
+                if(found >= 4)
+                {
+                    _BaseSocket.Receive(_Buffer, 0, 4, SocketFlags.None);
+                    int size = BitConverter.ToInt32(_Buffer, 0);
+                    int read = _BaseSocket.Receive(_Buffer, 0, size, SocketFlags.None);
+                    while(read < size)
+                    {
+                        read += _BaseSocket.Receive(_Buffer, read, size - read, SocketFlags.None);
+                    }
+                    process();
+                }
+            }
+            catch
+            {
+
+            }
+        }
+        internal void callProgressChanged(TransferQueue queue)
+        {
+            if (ProgressChanged != null)
+            {
+                ProgressChanged(this, queue);
+            }
+        }
+    }
+}
---
+++ DustDrop/TransferQueue.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DustDrop
+{
+    public enum QueueType : byte
+    {
+        Download,
+        Upload
+
+    }
+    public class TransferQueue
+    {
+        private const int FILE_BUFFER_SIZE = 8175;
+        private static byte[] file_buffer = new byte[FILE_BUFFER_SIZE];
+
+        private ManualResetEvent pauseEvent;
+
+        public int ID;
+        public int Progress, LastProgress;
+
+        public long Transferred;
+        public long Index;
+        public long Length;
+        public bool Running;
+        public bool Paused;
+
+        public string Filename;
+
+        public QueueType Type;
+        public TransferClient Client;
+        public Thread Thread;
+        public FileStream FS;
+
+        private TransferQueue()
+        {
+            pauseEvent = new ManualResetEvent(true);
+            
+            Running = true;
+        }
+
+        public void Start()
+        {
+            Running = true;
+            Thread.Start(this);
+        }
+
+        public void Stop()
+        {
+            Running = false;
+        }
+
+        public void Pause()
+        {
+            if (!Paused)
+            {
+                pauseEvent.Reset();
+            } else
+            {
+                pauseEvent.Set();
+            }
+            Paused = !Paused;
+        }
+
+        public void Close()
+        {
+            try
+            {
+                Client.Transfers.Remove(ID);
+            }
+            catch
+            {
+
+            }
+            Running = false;
+            FS.Close();
+            pauseEvent.Dispose();
+            Client = null;
+        }
+
+        public void Write(byte[] bytes, long index)
+        {
+            lock (this)
+            {
+                FS.Position = index;
+                FS.Write(bytes, 0, bytes.Length);
+                Transferred += bytes.Length;
+            }
+        }
+
+        private static void transferProc(object o)
+        {
+            TransferQueue queue = (TransferQueue)o;
+            while(queue.Running && queue.Index < queue.Length)
+            {
+                queue.pauseEvent.WaitOne();
+                if (!queue.Running)
+                {
+                    break;
+                }
+                lock (file_buffer)
+                {
+                    queue.FS.Position = queue.Index;
+                    int read = queue.FS.Read(file_buffer, 0, file_buffer.Length);
+                    PacketWriter pw = new PacketWriter();
+                    pw.Write(queue.ID);
+                    pw.Write(queue.Index);
+                    pw.Write(read);
+                    pw.Write(file_buffer, 0, read);
+                    queue.Transferred += read;
+                    queue.Index += read;
+
+                    queue.Client.Send(pw.GetBytes());
+                    queue.Progress = (int)((queue.Transferred * 100) / queue.Length);
+                    if(queue.LastProgress < queue.Progress)
+                    {
+                        queue.LastProgress = queue.Progress;
+                        queue.Client.callProgressChanged(queue);
+                    }
+                }
+            }
+        }
+    }
+}
